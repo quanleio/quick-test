f761e74f9a0cf63cb32008110a4594fe03f0c5c1
Le Hong Quan <quan.le@letsee.io>
2022-12-14 16:15:31 +0900

    fix: remove unused resources.

--- grain-effect/src/Experience/sources.js
+++ grain-effect/src/Experience/sources.js
@@ -1,18 +1,8 @@
 export default [
   {
-    name: "testMatcap",
-    type: "texture",
-    path: "assets/textures/matcap.png",
-  },
-  {
     name: "sceneBackground",
     type: "texture",
     path: "assets/textures/grey-gradient.png",
-  },
-  {
-    name: "foxModel",
-    type: "gltfModel",
-    path: "assets/models/Fox/glTF/Fox.gltf",
   },
   {
     name: "statueOfLiberty",
@@ -23,10 +13,5 @@ export default [
     name: "rockyGround",
     type: "gltfModel",
     path: "assets/models/rocky_ground_with_moss.glb",
-  },
-  {
-    name: "deerModel",
-    type: "objModel",
-    path: "assets/models/deer.obj",
   },
 ];

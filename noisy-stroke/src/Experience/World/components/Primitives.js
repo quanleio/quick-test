af0cafe186f839197672799da365a779202f109c
Le Hong Quan <quan.le@letsee.io>
2022-12-28 18:56:56 +0900

    animate primitives.

--- noisy-stroke/src/Experience/World/components/Primitives.js
+++ noisy-stroke/src/Experience/World/components/Primitives.js
@@ -33,14 +33,26 @@ export default class Primitives {
     return mesh
   }
   setObjects = () => {
+    this.groupMesh = new THREE.Object3D()
+    this.groupMesh.position.set(0, -50, 0)
+    this.scene.add(this.groupMesh)
+
     for(let i=0; i<this.count; i++) {
       const geo = this.geometries[Math.floor(Math.random() * 3 + 1)]
       const mesh = this.getMesh(geo.geometry, this.noiseMaterial.clone())
+      mesh.speed = Math.random() / 5
 
-      let xPos = THREE.MathUtils.randFloat(-1, 1);
+      /*let xPos = THREE.MathUtils.randFloat(-1, 1);
       let yPos = THREE.MathUtils.randFloat(-50, -10);
+      let zPos = THREE.MathUtils.randFloat(-1, 1);*/
+
+      let xPos = THREE.MathUtils.randFloat(-2, 2);
+      let yPos = THREE.MathUtils.randFloat(-1, 1);
       let zPos = THREE.MathUtils.randFloat(-1, 1);
       mesh.position.set(xPos, yPos, zPos)
+
+      const scaleFactor = THREE.MathUtils.randFloat(1, 3.5)
+      mesh.scale.setScalar(scaleFactor)
 
       mesh.rotation.set(geo.rotationX, geo.rotationY, geo.rotationZ)
 
@@ -49,13 +61,14 @@ export default class Primitives {
         y: mesh.rotation.y,
         z: mesh.rotation.z,
       }
-      this.scene.add(mesh)
+      // this.scene.add(mesh)
+      this.groupMesh.add(mesh)
       this.meshes.push(mesh)
     }
   }
   animate = () => {
-    /*const tl = gsap.timeline()
-    tl.to(this.sphere.position, {
+    const tl = gsap.timeline()
+    /*tl.to(this.sphere.position, {
       duration: 2.,
       y: 0,
       ease: "back.inOut(0.7)",
@@ -64,20 +77,37 @@ export default class Primitives {
       }
     })*/
 
-    for(let i=0; i<this.count; i++){
+    tl.to(this.groupMesh.position, {
+      duration: 1.8,
+      y: 0,
+      ease: "back.inOut(0.7)",
+      onComplete: () => {
+        this.isCompleted = true
+      }
+    })
+    for(let i=0; i<this.count; i++) {
       let mesh = this.meshes[i]
+
+      gsap.to(mesh.rotation, {
+        duration: 3.,
+        x: map(mesh.position.y, -1, 1, radians(45), mesh.initialRotation.x),
+        y: map(mesh.position.y, -1, 1, radians(-90), mesh.initialRotation.y),
+        z: map(mesh.position.y, -1, 1, radians(90), mesh.initialRotation.z),
+        ease: "back.inOut(0.7)",
+      })
+    }
+
+    /*for(let i=0; i<this.count; i++){
+      let mesh = this.meshes[i]
+      const scaleFactor = THREE.MathUtils.randFloat(0.5, 3.5)
 
       gsap.to(mesh.position, {
         duration: 1.8,
         x: THREE.MathUtils.randFloat(-2, 2),
         y: THREE.MathUtils.randFloat(-1, 1),
         ease: "back.inOut(0.7)",
-        onComplete: () => {
-          this.isCompleted = true
-        }
       })
 
-      const scaleFactor = THREE.MathUtils.randFloat(0.5, 3.5)
       gsap.to(mesh.scale, {
         duration: 1.8,
         x: scaleFactor,
@@ -98,7 +128,7 @@ export default class Primitives {
     // need to change to better option
     setTimeout(() => {
       this.isCompleted = true
-    }, 2000)
+    }, 2000)*/
 
   }
   update = () => {
@@ -115,11 +145,12 @@ export default class Primitives {
     for(let i=0; i<this.meshes.length; i++) {
       let mesh = this.meshes[i]
 
-      mesh.material.uniforms.uTime.value = performance.now() / 1500
+      mesh.material.uniforms.uTime.value = performance.now() / 1000
       let val = mesh.material.uniforms.uProgress.value
       if (this.isCompleted) {
         if (val >= 0 && val < 1.0) {
-          mesh.material.uniforms.uProgress.value = this.clock.getElapsedTime() * 0.2
+          // mesh.material.uniforms.uProgress.value = this.clock.getElapsedTime() * 0.2
+          mesh.material.uniforms.uProgress.value = this.clock.getElapsedTime() * mesh.speed // random speed for each mesh
         }
       }
     }

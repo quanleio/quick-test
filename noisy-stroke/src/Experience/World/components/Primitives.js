ce2425d8d35d38e60f5ad03d4a2f4c8bb6b696be
Le Hong Quan <quan.le@letsee.io>
2022-12-28 18:19:30 +0900

    wip: animate primitives.

--- noisy-stroke/src/Experience/World/components/Primitives.js
+++ noisy-stroke/src/Experience/World/components/Primitives.js
@@ -1,15 +1,23 @@
 import * as THREE from 'three'
 import { gsap } from 'gsap'
 import Experience from '../../Experience'
+import Torus from './Torus';
+import Cone from './Cone';
+import Box from './Box';
+import Cylinder from './Cylinder';
+import {map, radians} from '../../../../../pick-chocolate/src/utils/utils';
 
 export default class Primitives {
   constructor(_material) {
     this.experience = new Experience()
     this.scene = this.experience.scene
     this.clock = new THREE.Clock()
-    this.noiseMaterial = _material.clone()
+    this.noiseMaterial = _material
 
+    this.geometries = [new Box(), new Torus(), new Cone(), new Cylinder()]
     this.isCompleted = false
+    this.count = 5
+    this.meshes = []
 
     this.setObjects()
 
@@ -18,45 +26,101 @@ export default class Primitives {
     //   this.animate()
     // })
   }
+  getMesh = (geometry, material) => {
+    const mesh = new THREE.Mesh(geometry, material)
+    mesh.castShadow = mesh.receiveShadow = true
+
+    return mesh
+  }
   setObjects = () => {
-    const geometry = new THREE.SphereGeometry(2, 64, 64)
+    for(let i=0; i<this.count; i++) {
+      const geo = this.geometries[Math.floor(Math.random() * 3 + 1)]
+      const mesh = this.getMesh(geo.geometry, this.noiseMaterial.clone())
 
-    // sphere
-    this.sphere = new THREE.Mesh(geometry, this.noiseMaterial)
-    this.sphere.position.set(0, -50, 0)
-    this.scene.add(this.sphere)
+      let xPos = THREE.MathUtils.randFloat(-1, 1);
+      let yPos = THREE.MathUtils.randFloat(-50, -10);
+      let zPos = THREE.MathUtils.randFloat(-1, 1);
+      mesh.position.set(xPos, yPos, zPos)
 
-    // for test
-    // const cube = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial())
-    // cube.position.x = 5
-    // this.scene.add(cube)
+      mesh.rotation.set(geo.rotationX, geo.rotationY, geo.rotationZ)
+
+      mesh.initialRotation = {
+        x: mesh.rotation.x,
+        y: mesh.rotation.y,
+        z: mesh.rotation.z,
+      }
+      this.scene.add(mesh)
+      this.meshes.push(mesh)
+    }
   }
   animate = () => {
-    const tl = gsap.timeline()
+    /*const tl = gsap.timeline()
     tl.to(this.sphere.position, {
       duration: 2.,
       y: 0,
       ease: "back.inOut(0.7)",
       onComplete: () => {
         this.isCompleted = true
-        // this.sphere.material.uniforms.uProgress.value = 0.5
-        // this.noiseMaterial.uniforms.needsUpdate = true
       }
-    })
-    /*tl.to(this.sphere.position, {
-      duration: 2.,
-      y: -50,
-      ease: "back.inOut(0.7)",
     })*/
+
+    for(let i=0; i<this.count; i++){
+      let mesh = this.meshes[i]
+
+      gsap.to(mesh.position, {
+        duration: 1.8,
+        x: THREE.MathUtils.randFloat(-2, 2),
+        y: THREE.MathUtils.randFloat(-1, 1),
+        ease: "back.inOut(0.7)",
+        onComplete: () => {
+          this.isCompleted = true
+        }
+      })
+
+      const scaleFactor = THREE.MathUtils.randFloat(0.5, 3.5)
+      gsap.to(mesh.scale, {
+        duration: 1.8,
+        x: scaleFactor,
+        y: scaleFactor,
+        z: scaleFactor,
+        ease: "Expo.easeOut"
+      })
+
+      gsap.to(mesh.rotation, {
+        duration: .5,
+        x: map(mesh.position.y, -1, 1, radians(45), mesh.initialRotation.x),
+        y: map(mesh.position.y, -1, 1, radians(-90), mesh.initialRotation.y),
+        z: map(mesh.position.y, -1, 1, radians(90), mesh.initialRotation.z),
+        ease: "Expo.easeOut",
+      })
+    }
+
+    // need to change to better option
+    setTimeout(() => {
+      this.isCompleted = true
+    }, 2000)
+
   }
   update = () => {
-    this.noiseMaterial.uniforms.uTime.value = performance.now() / 1500
+    /*this.noiseMaterial.uniforms.uTime.value = performance.now() / 1500
 
     if (this.isCompleted) {
-      let val = this.sphere.material.uniforms.uProgress.value
+      let val = this.noiseMaterial.uniforms.uProgress.value
 
       if (val >= 0 && val < 1.0) {
-        this.sphere.material.uniforms.uProgress.value = this.clock.getElapsedTime() * 0.2
+        this.noiseMaterial.uniforms.uProgress.value = this.clock.getElapsedTime() * 0.2
+      }
+    }*/
+
+    for(let i=0; i<this.meshes.length; i++) {
+      let mesh = this.meshes[i]
+
+      mesh.material.uniforms.uTime.value = performance.now() / 1500
+      let val = mesh.material.uniforms.uProgress.value
+      if (this.isCompleted) {
+        if (val >= 0 && val < 1.0) {
+          mesh.material.uniforms.uProgress.value = this.clock.getElapsedTime() * 0.2
+        }
       }
     }
   }
